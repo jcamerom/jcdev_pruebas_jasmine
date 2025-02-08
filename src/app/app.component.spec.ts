0d185145513fc38af693635c27511bf3698f9a29
jcamerom <juan.camero.m@gmail.com>
2025-02-08 13:30:24 +0100

    Nuevos métodos

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -37,36 +37,53 @@ describe('AppComponent', () => {
   // });
 
   it('should render div with css classes', () => {
-    const divElement = compiled.querySelector('div'); // Coge por defecto sólo el primer elemento <div>
-    // Se crea un string con las clases que queremos comprobar y se convierte em un array de strings por cada clase css
-    const cssClasses = 'min-w-screen min-h-screen bg-slate-600 flex items-center justify-center px-5 py-5'.split(' ');
+    // Encuentra el primer elemento <div> en el DOM.
+    const divElement = compiled.querySelector('div');
+    // Comprobamos que no sea nulo el divElement y luego comprueba que contiene las clases con un forEach.
+    expect(divElement).not.toBeNull();
 
-    // Comprobamos que el elemento <div> tiene las clases indicadas en el string cssClasses.
+    // Podríamos intentar:
+    // Creamos un string con todas las clases del elemento.
+    // const cssClasses = 'min-w-screen min-h-screen bg-slate-600 flex items-center justify-center px-5 py-5';
+
     // El divElement? tiene una interrogación para comprobar si tiene una clase determinada. Si es nulo, la prueba falla.
     // classlist devuelve un objeto DOMTokenList que contiene los nombres de todas las clases del elemento.
     // Pero también un value que devuelve un string con todas las clases del elemento separadas por espacios.
+    // expect(divElement?.classList.value).toBe(cssClasses);
+    // Pero no es buena práctica. Es muy rígido. Si cambian las clases, la prueba fallará.
 
-    // Podríamos intentar:
-    // expect( divElement?.classList.value).toBe(cssClasses); // Pero no es buena práctica. Es muy rígido.
-
-    // Alternativa:
-    // Comprueba primero que no sea nulo el divElement y luego comprueba que contiene las clases.
+    // Alternativa dinámica:
+    // El string se convierte em un array de strings por cada clase css con ".split(' ')".
+    // const cssClasses = 'min-w-screen min-h-screen bg-slate-600 flex items-center justify-center px-5 py-5'.split(' ');
+    // Comprueba primero que no sea nulo el divElement y luego comprueba que contiene las clases con un forEach.
     // divElement?.classList.forEach((className) => {
     //   expect(cssClasses).toContain(className);
     // });
+    // Pero también es muy rígido. Si se agregan mas clases, la prueba fallará.
 
-    // Otra alternativa:
-    // Extraemos las clases del div con divClasses.
-    // Extraemos las clases del div y las compara con el array cssClasses.
-    const divClasses = divElement?.classList.value.split(' ');
-
-    expect(divElement).not.toBeNull();
-
+    // Mejor alternativa:
+    // Extraemos las clases del div con divClasses. Como antes pasaba que no era nulo,
+    // ahora podemos usar la exclamación para ver si contiene la clase.
+    const cssClasses = divElement!.classList.value.split(' ');
     cssClasses.forEach((className) => {
-      expect(divClasses).toContain(className);
+      expect(divElement!.classList).toContain(className);
     });
+  });
 
-
+  it('should contain the -buy me a beer link-', ()=> {
+    // Buscamos el segundo elemento de la lista de elementos <div> y extraemos el enlace con title="Buy me a beer" y href="https://www.buymeac
+    const divElement = compiled.querySelectorAll('div')[1];
+    // Aquí seleccionamos el elemento "a" que contiene el title "Buy me a beer"
+    const anchorElement = divElement?.querySelector('a');
+    // Comprobamos que el enlace no es nulo
+    expect(divElement).not.toBeNull();
+    // Comprobamos que el title del enlace es "Buy me a beer"
+    expect(anchorElement?.title).toEqual('Buy me a beer');
+    // Comprobamos que el href del enlace es "https://www.buymeacoffee.com/scottwindon"
+    expect(anchorElement?.getAttribute('href')).toEqual('https://www.buymeacoffee.com/scottwindon');
 
   });
+
+
+
 });
